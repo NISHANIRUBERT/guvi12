e6568d19855ff1012ddaeff0a7f2fc2dfb55481f
NISHANIRUBERT <51689306+NISHANIRUBERT@users.noreply.github.com>
2019-07-19 19:14:11 +0530

    Create stringwithoutvowels.c

---
+++ stringwithoutvowels.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	// your code goes here
+	int index,index2=0;
+	char str[100001],str2[100001];
+	scanf("%s",str);
+	index=strlen(str)-1;	
+	while(index>=0)
+	{
+		if(strchr("aeiouAEIOU",str[index]))
+		index--;
+		else
+		{
+			str2[index2++]=str[index];
+			index--;
+		}
+	}
+	str2[index2]='\0';
+	printf("%s",str2);
+	return 0;
+}
