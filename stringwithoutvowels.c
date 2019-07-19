fe7e7316ce5ad90cfbdcb8a00e3d4f77a86128a7
NISHANIRUBERT <51689306+NISHANIRUBERT@users.noreply.github.com>
2019-07-19 19:26:51 +0530

    Update stringwithoutvowels.c

--- stringwithoutvowels.c
+++ stringwithoutvowels.c
@@ -2,14 +2,17 @@
 #include<string.h>
 int main(void) {
 	// your code goes here
-	int index,index2=0;
+	int index,index2=0,n;
 	char str[100001],str2[100001];
-	scanf("%[^\n]s ",str);
+	scanf("%d\n",&n);
+	scanf("%[^\n]s",str);
 	index=strlen(str)-1;	
 	while(index>=0)
 	{
 		if(strchr("aeiouAEIOU",str[index]))
+		{
 		index--;
+		}
 		else
 		{
 			str2[index2++]=str[index];
